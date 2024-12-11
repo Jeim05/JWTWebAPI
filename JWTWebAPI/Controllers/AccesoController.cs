daa00d26d78b8f00768763a28e006e7172ec85a3
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-10 21:26:26 -0600

    DTO para login

---
+++ JWTWebAPI/Controllers/AccesoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JWTWebAPI.Custom;
+using JWTWebAPI.Models;
+using JWTWebAPI.Models.DTOs;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JWTWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous] //Indica que se puede acceder sin estar registrado
+    [ApiController]
+    public class AccesoController : ControllerBase
+    {
+    }
+}
---
+++ JWTWebAPI/Models/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTWebAPI.Models.DTOs
+{
+    public class LoginDTO
+    {
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+    }
+}
