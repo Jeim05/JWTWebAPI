f3aac84a0e342d07b3010082f379db6a0c255baf
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-10 21:43:29 -0600

    Implemntacion de controlador para el login y registro de Usuarios

--- JWTWebAPI/Controllers/AccesoController.cs
+++ JWTWebAPI/Controllers/AccesoController.cs
@@ -13,5 +13,50 @@ namespace JWTWebAPI.Controllers
     [ApiController]
     public class AccesoController : ControllerBase
     {
+        private readonly BdapijwtContext _context;
+        private readonly Utilidades _utilidades;
+
+        public AccesoController(BdapijwtContext context, Utilidades utilidades)
+        {
+            _context = context;
+            _utilidades = utilidades;
+        }
+
+        [HttpPost]
+        [Route("Registrarse")]
+        public async Task<IActionResult> Registrarse(UsuarioDTO objeto)
+        {
+            var modeloUsario = new Usuario
+            {
+                Nombre = objeto.Nombre,
+                Correo = objeto.Correo,
+                Clave = _utilidades.encriptarSHA256(objeto.Clave)
+            };
+
+            await _context.Usuarios.AddAsync(modeloUsario);
+            await _context.SaveChangesAsync();
+
+            if (modeloUsario.IdUsuario != 0)
+                return StatusCode(StatusCodes.Status200OK, new {isSuccess = true });
+            else 
+                return StatusCode(StatusCodes.Status200OK, new { isSuccess = false });
+
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(UsuarioDTO objeto)
+        {
+            var usuarioEncontrado = await _context.Usuarios
+                .Where(u=>
+                u.Correo==objeto.Correo &&
+                u.Clave == _utilidades.encriptarSHA256(objeto.Clave)
+                ).FirstOrDefaultAsync();
+
+            if(usuarioEncontrado == null)
+                return StatusCode(StatusCodes.Status200OK, new {isSucces=false, token = ""});
+            else
+                return StatusCode(StatusCodes.Status200OK, new { isSucces = false, token = _utilidades.generarJWT(usuarioEncontrado) });
+        }
     }
 }
