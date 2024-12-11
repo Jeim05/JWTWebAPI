5dcec482d0894f305ff4231ca875a518c413d5f6
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-10 22:00:42 -0600

    Controlador para listar los productos

---
+++ JWTWebAPI/Controllers/ProductoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JWTWebAPI.Custom;
+using JWTWebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JWTWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize()] // Solo pueden acceder usuarios Autorizados
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly BdapijwtContext _context;
+
+        public ProductoController(BdapijwtContext context, Utilidades utilidades)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var lista = await _context.Productos.ToListAsync();
+            return StatusCode(StatusCodes.Status200OK, new {value = lista});
+
+        }
+        
+        }
+}
